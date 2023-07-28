c44f3a531a3f1eb4674ee4293df8e4fc6c927ae5
drkop <satanatyn@yandex.ru>
2023-07-28 17:06:09 +0300

    hwoop6

---
+++ Work3/Greeter.java
@@ -0,0 +1,25 @@
+package Work3;
+
+public class Greeter {
+    private String formality;
+
+
+    public String greet(String greet) {
+       return this.formality = getGreet(greet);
+    }
+
+
+    public String getFormality() {
+        return formality;
+    }
+
+    public void setFormality(String formality) {
+        this.formality = formality;
+    }
+
+    @Override
+    public String toString() {
+        return formality;
+    }
+
+}
